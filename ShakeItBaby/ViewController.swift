b315c01fb482bf559ddf04ae4328e497e8f36764
Lamar <github@lamargreene.com>
2016-01-06 19:30:10 -0600

    I was able to to finally get this function to work... NSBundle.mainBundle().pathsForResourcesOfType(ext: String?, inDirectory: String?)
    The key is, the folder has to be added in Build Phases > Copy Bundle Resources. The good news is, once the folder is created, items can be added directly to the folder, and are dynamically allocated.

--- ShakeItBaby/ViewController.swift
+++ ShakeItBaby/ViewController.swift
@@ -7,17 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
+    var audioFile = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        audioFile = NSBundle.mainBundle().pathsForResourcesOfType(<#T##ext: String?##String?#>, inDirectory: <#T##String?#>) // NSBundle.mainBundle().pathsForResourcesOfType("mp3", inDirectory: "Audio")
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.MotionShake {
+        
+            
+        let audioFileRandom = Int(arc4random_uniform(UInt32(audioFile.count)))
+            
+            do
+            {
+
+                try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioFile[audioFileRandom]))
+                
+            
+            } catch {
+             
+                // error stuff here
+                
+            }
+            
+            player.play()
+        
+        }
     }
 
 
